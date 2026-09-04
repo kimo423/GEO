15c07a260f29dcf8eb8c0e1636df167594c960d4
kimo423 <kimo423@users.noreply.github.com>
2026-09-04 22:26:36 +0800

    chore: release GEO 1.1.1 update test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.geo.ledger"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
